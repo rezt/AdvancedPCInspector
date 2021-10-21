07fd6a2f1a806cc98e40d047551f8c6929cb3d65
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-10-21 17:10:04 +0200

    Initial commit

---
+++ src/main/java/sample/Main.java
@@ -0,0 +1,36 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+//        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.setTitle("Hello World");
+//        primaryStage.setScene(new Scene(root, 300, 275));
+//        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+
+        SystemInfo systemInfo = new SystemInfo();
+        OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
+        HardwareAbstractionLayer hal = systemInfo.getHardware();
+        CentralProcessor cpu = hal.getProcessor();
+
+        System.out.println("Operating System: " + operatingSystem.toString());
+        System.out.println(cpu.getLogicalProcessorCount());
+        System.out.println(cpu.getMaxFreq());
+        launch(args);
+    }
+}
