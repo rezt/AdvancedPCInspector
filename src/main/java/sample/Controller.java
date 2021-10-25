73c6bd0636e38cc4e990a3548f6fe91bd14f406c
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-10-25 15:43:12 +0200

    add javafx support and mvc foundation

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -1,4 +1,19 @@
 package sample;
 
+import oshi.SystemInfo;
+import oshi.hardware.CentralProcessor;
+import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.OperatingSystem;
+
 public class Controller {
+
+    //Create all links to devices
+    public void init() {
+        SystemInfo systemInfo = new SystemInfo();
+        OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
+        HardwareAbstractionLayer hal = systemInfo.getHardware();
+        CentralProcessor cpu = hal.getProcessor();
+    }
+
+    
 }
