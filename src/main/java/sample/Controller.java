625093b9918546df19a416e46806029df2e5c92a
Piotr Ćwiertnia <32513700+rezt@users.noreply.github.com>
2021-11-02 20:17:38 +0100

    refactor code, fix looping bug

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -55,63 +55,35 @@ public class Controller {
     @FXML
     private Tab helpTabName;
 
-    CentralProcessor cpu;
+    
     int language;
+
     List<String> Dyski = new ArrayList<>();
+    CentralProcessor cpu;
     List<String> kartaGraficzna = new ArrayList<>();
     List<String> RAM = new ArrayList<>();
+    List<OSFileStore> osFileStores;
+    List<GraphicsCard> cards;
+    HardwareAbstractionLayer hardware;
+    GlobalMemory globalMemory;
+    List<PhysicalMemory> physicalMemories;
 
     //Create all links to devices 
     public void initialize() {
         language = 0;
         SystemInfo systemInfo = new SystemInfo();
         OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
-
         FileSystem fileSystem = operatingSystem.getFileSystem();
-        List<OSFileStore> osFileStores = ((FileSystem) fileSystem).getFileStores();
-
-        for (OSFileStore fileStore : osFileStores) {
-            Dyski.add("Opis: " + fileStore.getDescription() + "\n");
-            Dyski.add("Label: " + fileStore.getLabel() + "\n");
-            Dyski.add("Partycja: " + fileStore.getMount() + "\n");
-            Dyski.add("Nazwa: " + fileStore.getName() + "\n");
-            Dyski.add("System Plików: " + fileStore.getType() + "\n");
-            // Dyski.add("UUID: " + fileStore.getUUID() + "\n");
-            // Dyski.add("Volume: " + fileStore.getVolume() + "\n");
-            Dyski.add("Wolna przestrzeń: " + FormatUtil.formatBytes(fileStore.getFreeSpace()) + "\n");
-            Dyski.add("Całkowita przestrzeń: " + FormatUtil.formatBytes(fileStore.getTotalSpace()) + "\n");
-            //Dyski.add("Użytkowa przestrzeń: " + FormatUtil.formatBytes(fileStore.getUsableSpace()) + "\n");
-
-            HardwareAbstractionLayer hal = systemInfo.getHardware();
-
-            List<GraphicsCard> cards = hal.getGraphicsCards();
-            for (GraphicsCard karta : cards) {
-                kartaGraficzna.add("Name:" + karta.getName());
-                kartaGraficzna.add("Device ID:" + karta.getDeviceId());
-                kartaGraficzna.add("Vendor:" + karta.getVendor());
-                kartaGraficzna.add("Version:" + karta.getVersionInfo());
-                kartaGraficzna.add("Vram:" + karta.getVRam());
-            }
-
-
-            HardwareAbstractionLayer hardware = systemInfo.getHardware();
-            GlobalMemory globalMemory = hardware.getMemory();
-
-            List<PhysicalMemory> physicalMemories = globalMemory.getPhysicalMemory();
-            for (PhysicalMemory physicalMemory : physicalMemories) {
-                RAM.add("Manufacturer: " + physicalMemory.getManufacturer());
-                RAM.add("Memory type: " + physicalMemory.getMemoryType());
-                RAM.add("Bank/slot label: " + physicalMemory.getBankLabel());
-                RAM.add("Capacity: " + FormatUtil.formatBytes(physicalMemory.getCapacity()));
-                RAM.add("Clock speed: " + FormatUtil.formatHertz(physicalMemory.getClockSpeed()));
-            }
-
-            cpu = hal.getProcessor();
-
-            setInfo();
+        osFileStores = ((FileSystem) fileSystem).getFileStores();
+        HardwareAbstractionLayer hardware = systemInfo.getHardware();
+        cards = hardware.getGraphicsCards();
+        globalMemory = hardware.getMemory();
+        physicalMemories = globalMemory.getPhysicalMemory();
+        cpu = hardware.getProcessor();
+        setInfo();
         }
 
-    }
+
 
     public void setInfo() {
         setCPUInfo();
@@ -133,6 +105,14 @@ public class Controller {
     }
 
     public void setGPUInfo() {
+        for (GraphicsCard karta : cards) {
+            kartaGraficzna.add("Name:" + karta.getName());
+            kartaGraficzna.add("Device ID:" + karta.getDeviceId());
+            kartaGraficzna.add("Vendor:" + karta.getVendor());
+            kartaGraficzna.add("Version:" + karta.getVersionInfo());
+            kartaGraficzna.add("Vram:" + karta.getVRam());
+            kartaGraficzna.add("--------------------------------------------");
+        }
         for (Object g : kartaGraficzna) {
             gpuTextArea.appendText(g + "\n");
         }
@@ -143,6 +123,14 @@ public class Controller {
     }
 
     public void setRamInfo() {
+        for (PhysicalMemory physicalMemory : physicalMemories) {
+            RAM.add("Manufacturer: " + physicalMemory.getManufacturer());
+            RAM.add("Memory type: " + physicalMemory.getMemoryType());
+            RAM.add("Bank/slot label: " + physicalMemory.getBankLabel());
+            RAM.add("Capacity: " + FormatUtil.formatBytes(physicalMemory.getCapacity()));
+            RAM.add("Clock speed: " + FormatUtil.formatHertz(physicalMemory.getClockSpeed()));
+            RAM.add("--------------------------------------------");
+        }
         for (Object h : RAM) {
             ramTextArea.appendText(h + "\n");
         }
@@ -153,6 +141,19 @@ public class Controller {
     }
 
     public void setHardDriveInfo() {
+        for (OSFileStore fileStore : osFileStores) {
+            Dyski.add("Opis: " + fileStore.getDescription() + "\n");
+            Dyski.add("Label: " + fileStore.getLabel() + "\n");
+            Dyski.add("Partycja: " + fileStore.getMount() + "\n");
+            Dyski.add("Nazwa: " + fileStore.getName() + "\n");
+            Dyski.add("System Plików: " + fileStore.getType() + "\n");
+            // Dyski.add("UUID: " + fileStore.getUUID() + "\n");
+            // Dyski.add("Volume: " + fileStore.getVolume() + "\n");
+            Dyski.add("Wolna przestrzeń: " + FormatUtil.formatBytes(fileStore.getFreeSpace()) + "\n");
+            Dyski.add("Całkowita przestrzeń: " + FormatUtil.formatBytes(fileStore.getTotalSpace()) + "\n");
+            //Dyski.add("Użytkowa przestrzeń: " + FormatUtil.formatBytes(fileStore.getUsableSpace()) + "\n");
+            Dyski.add("--------------------------------------------");
+        }
         for (Object o : Dyski) {
             hardDriveTextArea.appendText(o + "\n");
         }
