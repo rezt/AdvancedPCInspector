e9841fd3f8acf8cbb52de394e911e37f317a6c46
Marek Naglik <63313452+lego31318@users.noreply.github.com>
2021-11-02 19:26:37 +0100

    Dyski
    hehe

--- src/main/java/sample/Controller.java
+++ src/main/java/sample/Controller.java
@@ -6,7 +6,13 @@ import javafx.scene.control.TextArea;
 import oshi.SystemInfo;
 import oshi.hardware.CentralProcessor;
 import oshi.hardware.HardwareAbstractionLayer;
+import oshi.software.os.FileSystem;
+import oshi.software.os.OSFileStore;
 import oshi.software.os.OperatingSystem;
+import oshi.util.FormatUtil;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Controller {
 
@@ -20,7 +26,7 @@ public class Controller {
     @FXML
     private Tab motherboardTabName;
 
-    @FXML 
+    @FXML
     private TextArea cpuTextArea;
     @FXML
     private Tab cpuTabName;
@@ -52,15 +58,37 @@ public class Controller {
 
     CentralProcessor cpu;
     int language;
+    List<String> Dyski = new ArrayList<>();
 
     //Create all links to devices 
     public void initialize() {
         language = 0;
         SystemInfo systemInfo = new SystemInfo();
         OperatingSystem operatingSystem = systemInfo.getOperatingSystem();
-        HardwareAbstractionLayer hal = systemInfo.getHardware();
-        cpu = hal.getProcessor();
-        setInfo();
+
+        FileSystem fileSystem = operatingSystem.getFileSystem();
+        List<OSFileStore> osFileStores = ((FileSystem) fileSystem).getFileStores();
+
+        for (OSFileStore fileStore : osFileStores) {
+            Dyski.add("Description: " + fileStore.getDescription() + "\n");
+            Dyski.add("Label: " + fileStore.getLabel() + "\n");
+            Dyski.add("Logical Volume: " + fileStore.getLogicalVolume() + "\n");
+            Dyski.add("Mount: " + fileStore.getMount() + "\n");
+            Dyski.add("Name: " + fileStore.getName() + "\n");
+            Dyski.add("Options: " + fileStore.getOptions() + "\n");
+            Dyski.add("Type: " + fileStore.getType() + "\n");
+            Dyski.add("UUID: " + fileStore.getUUID() + "\n");
+            Dyski.add("Volume: " + fileStore.getVolume() + "\n");
+            Dyski.add("Free Space: " + FormatUtil.formatBytes(fileStore.getFreeSpace()) + "\n");
+            Dyski.add("Total Space: " + FormatUtil.formatBytes(fileStore.getTotalSpace()) + "\n");
+            Dyski.add("Usable Space: " + FormatUtil.formatBytes(fileStore.getUsableSpace()) + "\n");
+
+            HardwareAbstractionLayer hal = systemInfo.getHardware();
+            cpu = hal.getProcessor();
+
+            setInfo();
+        }
+
     }
 
     public void setInfo() {
@@ -74,6 +102,44 @@ public class Controller {
         setHelpInfo();
     }
 
+    //All functions retriving informations about system and devices:
+    public void setCPUInfo() {
+        float maxFreq = (float) cpu.getMaxFreq();
+        maxFreq = maxFreq / 1000000000;
+        System.out.println(language);
+        cpuTextArea.setText(Constants.cpuFreqString[language] + Float.toString(maxFreq) + " GHZ");
+    }
+
+    public void setGPUInfo() {
+        gpuTextArea.setText("karta graficzna super mocna do krypto");
+    }
+
+    public void setGeneralInfo() {
+        generalTextArea.setText("Generalnie Informacje");
+    }
+
+    public void setRamInfo() {
+        ramTextArea.setText("Informacje o ramie");
+    }
+
+    public void setMotherboardInfo() {
+        motherboardTextArea.setText("Hello World!");
+    }
+
+    public void setHardDriveInfo() {
+        for (Object o : Dyski) {
+            hardDriveTextArea.appendText(o + "\n");
+        }
+    }
+
+    public void setNetworkInfo() {
+        networkTextArea.setText("Inne Informacje o SIECI");
+    }
+
+    public void setHelpInfo() {
+        helpTextArea.setText("Pomoc");
+    }
+
     public void setToEnglish() {
         language = 0;
         System.out.println(language);
@@ -85,53 +151,4 @@ public class Controller {
         System.out.println(language);
         setInfo();
     }
-
-    //All functions retriving informations about system and devices:
-    public void setCPUInfo() {
-        float maxFreq = (float)cpu.getMaxFreq();
-        maxFreq = maxFreq/1000000000;
-        System.out.println(language);
-        cpuTextArea.setText(Constants.cpuFreqString[language] + Float.toString(maxFreq) + " GHZ");
-    }
-
-    public void setGPUInfo() {
-        gpuTextArea.setText("karta graficzna super mocna do krypto");
-    }
-
-    public void setGeneralInfo()
-    {
-        generalTextArea.setText("Generalnie Informacje");
-    }
-
-    public void setRamInfo()
-    {
-        ramTextArea.setText("Informacje o ramie");
-    }
-
-    public void setMotherboardInfo()
-    {
-        motherboardTextArea.setText("Hello World!");
-    }
-
-    public void setHardDriveInfo()
-    {
-        hardDriveTextArea.setText("Informacje o dysku twardym");
-    }
-
-    public void setNetworkInfo()
-    {
-        networkTextArea.setText("Inne Informacje o SIECI");
-    }
-
-    public void setHelpInfo()
-    {
-        helpTextArea.setText("Pomoc");
-    }
-
-
-
-
-
-
-
 }
