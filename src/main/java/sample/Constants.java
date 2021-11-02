0529c4e319300ffaf16cf2395675881f9e3e3ba7
Marek Naglik <63313452+lego31318@users.noreply.github.com>
2021-11-02 19:55:42 +0100

    gg
    gg

--- src/main/java/sample/Constants.java
+++ src/main/java/sample/Constants.java
@@ -2,5 +2,9 @@ package sample;
 
 public class Constants {
     static String[] cpuFreqString = {"CPU maximum frequency: ","Maksymalne taktowanie CPU: "};
+    //static String[] hardDriveDescriptionString
+    //static String[] hardDriveLabelString
+    //static String[] hardDriveMount
+    //static String[] hardDriveName
 
 }
